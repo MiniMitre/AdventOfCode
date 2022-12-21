d0fdd33ef358b8a6f922615f742851ec536e4395
MiniMitre <34739837+MiniMitre@users.noreply.github.com>
2022-12-21 11:25:04 +0000

    Re-format css files

--- 2022/my-app/src/Home/Home.tsx
+++ 2022/my-app/src/Home/Home.tsx
@@ -5,12 +5,16 @@ import Logo from "./img/AdventOfCode_Logo.png"
 
 const Home: React.FC = () => {
   return(
-    <div className="App-body">
+    <div className="container">
+      <div className="home-logo">
       <img
         src={Logo} className="App-logo" alt="logo"/>
+      </div>
+      <div className="home-text">
       <p>
         Solving <a href="https://adventofcode.com">Advent Of Code</a> problems with React!
       </p>
+      </div>
     </div>
   )
 }
