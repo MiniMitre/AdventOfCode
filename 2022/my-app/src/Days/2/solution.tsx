b8897c966b540392c166271ccaae78793cc86f02
MiniMitre <34739837+MiniMitre@users.noreply.github.com>
2022-12-26 19:28:22 +0000

    Update highlighting of part 2 for all days

--- 2022/my-app/src/Days/2/solution.tsx
+++ 2022/my-app/src/Days/2/solution.tsx
@@ -251,7 +251,7 @@ return result
 }`
 
 //Add highlighting to code
-part2Code = hljs.highlight(part1Code,{language: 'TypeScript'}).value
+part2Code = hljs.highlight(part2Code,{language: 'TypeScript'}).value
 //Will need to use dangerouslySetInnerHTML but that is okay because I am not allowing user input strings
 
 const toggleExpand = (part : string) => {
