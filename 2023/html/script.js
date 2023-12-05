5de454f90984c2d9950551ab5b693bdf96d2d7ef
MiniMitre <34739837+MiniMitre@users.noreply.github.com>
2023-12-05 20:47:55 +0000

    Allow reading of files

--- 2023/html/script.js
+++ 2023/html/script.js
@@ -1,3 +1,21 @@
+// Function to fetch and display file content
+async function readFileContent() {
+  try {
+    const response = await fetch("http://localhost:3000/readfile/1");
+    if (!response.ok) {
+      throw new Error("Failed to fetch file");
+    }
+
+    const fileContent = await response.text();
+    console.log(fileContent);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+// Call the function when the page is loaded
+window.onload = readFileContent;
+
 if (document.body.classList.contains("page-1")) {
   console.log(".");
 }
--- 2023/server.js
+++ 2023/server.js
@@ -8,6 +8,25 @@ const port = 3000;
 // Serve static files from the 'html' directory
 app.use(express.static("html"));
 
+// Endpoint to read a file based on the day parameter
+app.get("/readFile/:day", (req, res) => {
+  const day = req.params.day;
+  const filename = `day-${day}.txt`; // Assuming your files are named day-1.txt, day-2.txt, etc.
+  const filePath = path.resolve(__dirname, "input", filename);
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err); // Log the error for debugging
+      res
+        .status(500)
+        .send({ error: "Error reading file", details: err.message });
+      return;
+    }
+
+    res.send(data);
+  });
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
